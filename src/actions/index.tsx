3cc353090e365295e66dc9e7bc355cc15aef792c
KarolRe <karolrewera+raycast@gmail.com>
2023-03-30 23:36:21 +0200

    Store tabs with windowId in getOpenTabs

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -15,19 +15,16 @@ export async function getOpenTabs(useOriginalFavicon: boolean): Promise<Tab[]> {
     const openTabs = await runAppleScript(`
       set _output to ""
       tell application "Google Chrome"
-        set _window_index to 1
         repeat with w in windows
+          set _w_id to get id of w  
           set _tab_index to 1
-          set _w_id to 
           repeat with t in tabs of w
             set _title to get title of t
             set _url to get URL of t
             set _favicon to ${faviconFormula}
-            set _output to (_output & _title & "${Tab.TAB_CONTENTS_SEPARATOR}" & _url & "${Tab.TAB_CONTENTS_SEPARATOR}" & _favicon & "${Tab.TAB_CONTENTS_SEPARATOR}" & _window_index & "${Tab.TAB_CONTENTS_SEPARATOR}" & _tab_index & "\\n")
+            set _output to (_output & _title & "${Tab.TAB_CONTENTS_SEPARATOR}" & _url & "${Tab.TAB_CONTENTS_SEPARATOR}" & _favicon & "${Tab.TAB_CONTENTS_SEPARATOR}" & _w_id & "${Tab.TAB_CONTENTS_SEPARATOR}" & _tab_index & "\\n")
             set _tab_index to _tab_index + 1
           end repeat
-          set _window_index to _window_index + 1
-          if _window_index > count windows then exit repeat
         end repeat
       end tell
       return _output
